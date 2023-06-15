743607d1eb0e298dd0bcfcda9a56ba519d6259df
pradeep-echortech <pradeep.p@echortech.com>
2023-06-15 10:04:20 +0530

    14-06

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
+const slugify = require('slugify')
+const replaceTemplate = require('./modules/replaceTemplate')
 
 // blocking
 // const textIn = fs.readFileSync('./txt/input.txt','utf-8')
@@ -21,20 +24,45 @@ const http = require('http')
 // })
 
 //Server
-const server = http.createServer((req,res)=>{
-    const pathName = req.url
-    if(pathName === '/' || pathName === '/overview'){
-        res.end('This is an Overview Page')
-    }else if(pathName === '/product') {
-        res.end('This is a Product Page')
-    }else{
-        res.writeHead(404,{
-            'content-type':'text/html'
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data)
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
+console.log(slugs)
+
+const server = http.createServer((req, res) => {
+    const { query, pathname } = url.parse(req.url, true)
+    // Overview page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output)
+        // Product page
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output)
+        // Api
+    } else if (pathname === '/api') {
+        res.writeHead(200, { 'content-type': 'application/jsom' })
+        res.end(data)
+        // Page not found
+    } else {
+        res.writeHead(404, {
+            'content-type': 'text/html'
         })
         res.end('<h1>Page not found !</h1>')
     }
 })
 
-server.listen(8000,()=>{
+server.listen(8000, () => {
     console.log('Listening to the requests on port 8000')
 })
