2ae978d94120055bb147c1980de960de4aa1d210
pradeep-echortech <pradeep.p@echortech.com>
2023-06-23 09:43:42 +0530

    22-06

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password'],
     minlength: 8,
+    select:false
   },
   passwordConfirm: {
     type: String,
